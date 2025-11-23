5d701dc4ef6959ef55ae73e25c1b3a0ae7dd37ef
Piotr Duma <piotrduma2014@gmail.com>
2025-11-23 21:04:15 +0100

    simplify User class

--- src/main/java/com/github/Piotr_Duma/utils/User.java
+++ src/main/java/com/github/Piotr_Duma/utils/User.java
@@ -34,21 +34,4 @@ public class User {
         ", password='" + password + '\'' +
         '}';
   }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    User user = (User) o;
-    return login.equals(user.login) && password.equals(user.password);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(login, password);
-  }
 }
