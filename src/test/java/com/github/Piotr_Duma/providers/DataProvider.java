760493954957925a8ef4a50af22fe40cefaf793d
Piotr Duma <piotrduma2014@gmail.com>
2025-11-24 03:22:04 +0100

    optimize test DataProvider

--- src/test/java/com/github/Piotr_Duma/providers/DataProvider.java
+++ src/test/java/com/github/Piotr_Duma/providers/DataProvider.java
@@ -8,21 +8,16 @@ import org.junit.jupiter.params.provider.Arguments;
 public class DataProvider {
 
   public static Stream<Arguments> getValidUserCredentials() {
-    Stream.Builder<Arguments> builder = Stream.builder();
-    builder.add(Arguments.of(new User("standard_user", "secret_sauce")));
-//    builder.add(Arguments.of(new User("locked_out_user", "secret_sauce"))); //invalid login
-    builder.add(Arguments.of(new User("problem_user", "secret_sauce")));
-    builder.add(Arguments.of(new User("performance_glitch_user", "secret_sauce")));
-//    builder.add(Arguments.of(new User("error_user", "secret_sauce"))); //invalid login
-    builder.add(Arguments.of(new User("visual_user", "secret_sauce")));
-    return builder.build();
+    return Stream.of(
+      Arguments.of(new User("standard_user", "secret_sauce")),
+      Arguments.of(new User("problem_user", "secret_sauce")),
+      Arguments.of(new User("performance_glitch_user", "secret_sauce")),
+      Arguments.of(new User("visual_user", "secret_sauce")));
   }
 
   public static Stream<Arguments> getInvalidUserCredentials() {
-    Stream.Builder<Arguments> builder = Stream.builder();
-    builder.add(Arguments.of(new User(StringGenerator.generateString(10), StringGenerator.generateString(10))));
-    builder.add(Arguments.of(new User(StringGenerator.generateString(10), StringGenerator.generateString(10))));
-    builder.add(Arguments.of(new User(StringGenerator.generateString(10), StringGenerator.generateString(10))));
-    return builder.build();
+    return Stream.of(
+      Arguments.of(new User(StringGenerator.generateString(10), StringGenerator.generateString(10))),
+      Arguments.of(new User(StringGenerator.generateString(10), "")));
   }
 }
